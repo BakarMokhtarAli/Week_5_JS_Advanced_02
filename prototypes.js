a293b232a48eab6314d295b8c71aac16e5503b93
bakar mokhtar <walaalpakar08@gmail.com>
2023-03-07 19:17:08 +0300

    Waan ka shaqeeyay

--- prototypes.js
+++ prototypes.js
@@ -10,3 +10,67 @@
 // Create an object named "Me" using the "Person" constructor function
 // Call the "Speak" method on the "Me" object
 // Console log all your results
+
+function Person(items){
+    this.name=items.name;
+    this.age=items.age;
+    this.hobby=items.hobby;
+}
+
+Person.prototype.speak=function(){
+    return `hello My name is ${this.name} and I'm ${this.age} Years old. I Love ${this.hobby}`
+}
+
+const me = new Person({
+    name:'Abuukar Macalain Mukhtar',
+    age:19,
+    hobby:'footBall'
+})
+
+
+
+function brother(items){
+    Person.call(this,items)
+}
+brother.prototype=Object.create(Person.prototype)
+
+const myBrother = new brother({
+    name:'Mohamed Macalin Mukhtar',
+    age:22,
+    hobby:'footBall',
+})
+
+
+function sisters(items){
+    brother.call(this,items)
+}
+
+sisters.prototype=Object.create(brother.prototype)
+const mySister = new sisters({
+    name:'Fatuma Macalin Mukhtar',
+    age:20,
+    hobby:'movies'
+})
+function Mom(items){
+    sisters.call(this, items)
+}
+Mom.prototype=Object.create(sisters.prototype)
+const myMom = new Mom({
+    name:'Aamino Maxamed Cali',
+    age:38,
+    hobby:'watching',
+})
+function Daddy(items){
+    Person.call(this, items)
+}
+Daddy.prototype=Object.create(Person.prototype)
+const myDad = new Daddy({
+    name:'Mukhtar Cali Barre',
+    age:39,
+    hobby:'Travelling'
+})
+console.log(me.speak())
+console.log(myBrother.speak())
+console.log(mySister.speak())
+console.log(myMom.speak())
+console.log(myDad.speak())
